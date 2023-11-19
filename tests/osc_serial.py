91605fa9daba4218b50fa9d2b3bc6c065ee8b7b5
Michael Schnebly <michael.schnebly@gmail.com>
2023-11-19 13:18:55 -0500

    add osc_serial example

--- tests/osc_serial.py
+++ tests/osc_serial.py
@@ -1,26 +1,30 @@
 import serial
-from pythonosc import osc_message_builder
-from pythonosc import osc_bundle_builder
-from pythonosc import osc_packet
+import sliplib
+from pythonosc.osc_message import OscMessage
 import time
 
-# Set up serial connection (adjust the COM port as needed)
-serial_port = '/dev/ttyUSB0'  # Example for Linux
-baud_rate = 115200
-ser = serial.Serial(serial_port, baud_rate)
+# Serial port configuration
+SERIAL_PORT = '/dev/cu.usbmodem14601'  # Update this with your serial port
+BAUD_RATE = 1000000              # Update this with your baud rate
 
-while True:
-    # Read from serial port
-    if ser.in_waiting > 0:
-        serial_data = ser.readline().decode('utf-8').strip()
+ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+t = 0
+try:
+    buffer = bytearray()
+    while True:
+        byte = ser.read()
 
-        # Parse the OSC message
-        try:
-            # OSC messages are typically binary, so you may need to adjust this
-            osc_msg = osc_packet.OscPacket(serial_data).messages
-            print("Received OSC message:", osc_msg)
-        except Exception as e:
-            print("Error parsing OSC message:", e)
+        if byte == sliplib.END:
+            
+            if len(buffer) > 0:
 
-        # Add a small delay to avoid high CPU usage
-        time.sleep(0.1)
+                osc_message = OscMessage(buffer)
+                print("Received OSC message:", osc_message.address, osc_message.params, "at", round(time.time()-t, 2), "seconds")
+                buffer.clear()
+                t = time.time()
+        else:
+            buffer.append(byte[0])
+
+except KeyboardInterrupt:
+    ser.close()
+    print("Serial connection closed.")
